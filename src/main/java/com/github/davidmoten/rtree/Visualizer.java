3b6da5595b763350bf09d87ec6f933386b9afcb1
Dave Moten <davidmoten@gmail.com>
2014-08-27 15:40:21 +1000

    use rx

--- src/main/java/com/github/davidmoten/rtree/Visualizer.java
+++ src/main/java/com/github/davidmoten/rtree/Visualizer.java
@@ -1,5 +1,6 @@
 package com.github.davidmoten.rtree;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,18 +8,18 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import com.google.common.base.Optional;
-
 public class Visualizer {
 
 	private final RTree tree;
 	private final int width;
 	private final int height;
+	private final Rectangle view;
 
 	public Visualizer(RTree tree, int width, int height, Rectangle view) {
 		this.tree = tree;
 		this.width = width;
 		this.height = height;
+		this.view = view;
 	}
 
 	public BufferedImage create() {
@@ -26,12 +27,35 @@ public class Visualizer {
 				BufferedImage.TYPE_INT_ARGB);
 		final Graphics2D g = (Graphics2D) image.getGraphics();
 		if (tree.root().isPresent())
-			drawNode(image, tree.root(), 0);
+			drawNode((Graphics2D) image.getGraphics(), tree.root().get(), 0);
 		return image;
 	}
 
-	private void drawNode(BufferedImage image, Optional<Node> node, int depth) {
+	private void drawNode(Graphics2D g, Node node, int depth) {
+		Color color = Color.getHSBColor(depth / 10f, 1f, 1f);
+		g.setColor(color);
+		Rectangle r = node.mbr();
+		drawRectangle(g, r);
+		if (node instanceof Leaf) {
+			g.setColor(Color.black);
+			Leaf leaf = (Leaf) node;
+			for (Entry entry : leaf.entries()) {
 
+			}
+		}
+
+	}
+
+	private void drawRectangle(Graphics2D g, Rectangle r) {
+		double x1 = (r.x1() - view.x1()) / (view.x2() - view.x1()) * width;
+		double y1 = (r.y1() - view.y1()) / (view.y2() - view.y1()) * height;
+		double x2 = (r.x2() - view.x1()) / (view.x2() - view.x1()) * width;
+		double y2 = (r.y2() - view.y1()) / (view.y2() - view.y1()) * height;
+		g.drawRect(rnd(x1), rnd(y1), rnd(x2 - x1), rnd(y2 - y1));
+	}
+
+	private static int rnd(double d) {
+		return (int) Math.round(d);
 	}
 
 	public void save(File file, String imageFormat) {
