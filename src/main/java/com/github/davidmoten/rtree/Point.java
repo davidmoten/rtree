f709b420609e6aa28e51937e32300f5d23d03cb9
Dave Moten <davidmoten@gmail.com>
2014-08-28 20:20:20 +1000

    add Point implementation of Geometry

---
+++ src/main/java/com/github/davidmoten/rtree/Point.java
@@ -0,0 +1,34 @@
+package com.github.davidmoten.rtree;
+
+public class Point implements Geometry {
+
+	private final Rectangle mbr;
+
+	public Point(float x, float y) {
+		this.mbr = Rectangle.create(x, y, x, y);
+	}
+
+	@Override
+	public Rectangle mbr() {
+		return mbr;
+	}
+
+	@Override
+	public double distance(Rectangle r) {
+		return mbr.distance(r);
+	}
+
+	@Override
+	public boolean intersects(Rectangle r) {
+		return mbr.intersects(r);
+	}
+
+	public float x() {
+		return mbr.x1();
+	}
+
+	public float y() {
+		return mbr.y1();
+	}
+
+}
