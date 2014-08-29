edceba5a76a6b455de6ef75734c6023b60a66950
Dave Moten <davidmoten@gmail.com>
2014-08-29 11:20:18 +1000

    add tests

--- src/main/java/com/github/davidmoten/rtree/Entry.java
+++ src/main/java/com/github/davidmoten/rtree/Entry.java
@@ -38,4 +38,36 @@ public class Entry<T> implements HasMbr {
 		return "Entry [object=" + object + ", geometry=" + geometry + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((geometry == null) ? 0 : geometry.hashCode());
+		result = prime * result + ((object == null) ? 0 : object.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entry<?> other = (Entry<?>) obj;
+		if (geometry == null) {
+			if (other.geometry != null)
+				return false;
+		} else if (!geometry.equals(other.geometry))
+			return false;
+		if (object == null) {
+			if (other.object != null)
+				return false;
+		} else if (!object.equals(other.object))
+			return false;
+		return true;
+	}
+
 }
--- src/test/java/com/github/davidmoten/rtree/RTreeTest.java
+++ src/test/java/com/github/davidmoten/rtree/RTreeTest.java
@@ -43,12 +43,8 @@ public class RTreeTest {
 	public void testPerformanceAndEntriesCount() {
 
 		long t = System.currentTimeMillis();
-		RTree<Object> tree = RTree.builder().maxChildren(4).build();
-		long n = 10000;
-		for (int i = 0; i < n; i++) {
-			Entry<Object> entry = new Entry<Object>(new Object(), random());
-			tree = tree.add(entry);
-		}
+		int n = 10000;
+		RTree<Object> tree = createRandomRTree(n);
 		long diff = System.currentTimeMillis() - t;
 		System.out.println("inserts/second = " + ((double) n / diff * 1000));
 		assertEquals(n, (int) tree.entries().count().toBlocking().single());
@@ -61,6 +57,15 @@ public class RTreeTest {
 		System.out
 				.println("time to get nearest with " + n + " entries=" + diff);
 
+	}
+
+	private static RTree<Object> createRandomRTree(int n) {
+		RTree<Object> tree = RTree.builder().maxChildren(4).build();
+		for (int i = 0; i < n; i++) {
+			Entry<Object> entry = new Entry<Object>(new Object(), random());
+			tree = tree.add(entry);
+		}
+		return tree;
 	}
 
 	@Test
@@ -76,14 +81,26 @@ public class RTreeTest {
 
 	@Test
 	public void testVisualizer() {
-		RTree<Object> tree = RTree.builder().maxChildren(4).build();
-		int n = 100;
-		for (int i = 0; i < n; i++) {
-			Entry<Object> entry = new Entry<Object>(new Object(), random());
-			tree = tree.add(entry);
-		}
+		RTree<Object> tree = createRandomRTree(100);
 		tree.visualize(600, 600, new Rectangle(-20, -20, 1100, 1100), 5).save(
 				new File("target/tree.png"), "PNG");
+	}
+
+	@Test
+	public void testDeleteOneFromOne() {
+		Entry<Object> e1 = e(1);
+		RTree<Object> tree = RTree.builder().maxChildren(4).build().add(e1)
+				.delete(e1);
+		assertEquals(0, (int) tree.entries().count().toBlocking().single());
+	}
+
+	@Test
+	public void testDeleteOneFromTreeWithDepthGreaterThanOne() {
+		Entry<Object> e1 = e(1);
+		RTree<Object> tree = RTree.builder().maxChildren(4).build().add(e1)
+				.add(e(2)).add(e(3)).add(e(4)).add(e(5)).add(e(6)).add(e(7))
+				.add(e(8)).add(e(9)).add(e(10)).delete(e1);
+		assertEquals(9, (int) tree.entries().count().toBlocking().single());
 	}
 
 	private static Entry<Object> e(int n) {
