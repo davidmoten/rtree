3ec682c06b88b2b9ca83f7567e237bd0efe34b74
Dave Moten <davidmoten@gmail.com>
2014-08-29 13:23:54 +1000

    add javadoc

--- src/main/java/com/github/davidmoten/rtree/geometry/Geometry.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/Geometry.java
@@ -1,10 +1,32 @@
 package com.github.davidmoten.rtree.geometry;
 
+import com.github.davidmoten.rtree.Entry;
 
+/**
+ * A geometrical region that represents an Entry spatially. It is recommended
+ * that implementations of this interface implement equals() and hashCode()
+ * appropriately that {@link Entry} equality checks work as expected.
+ */
 public interface Geometry extends HasMbr {
 
+	/**
+	 * Returns the distance to the given {@link Rectangle}. For a
+	 * {@link Rectangle} this might be Euclidean distance but for an EPSG4326
+	 * lat-long Rectangle might be great-circle distance.
+	 * 
+	 * @param r
+	 *            rectangle to measure distance to
+	 * @return distance to the rectangle r from the geometry
+	 */
 	double distance(Rectangle r);
 
+	/**
+	 * Returns true if and only if the geometry intersect the given
+	 * {@link Rectangle}.
+	 * 
+	 * @param r
+	 * @return
+	 */
 	boolean intersects(Rectangle r);
 
 }
