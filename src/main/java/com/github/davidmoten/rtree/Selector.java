dc93fdc0ea8da0c4492b6e37b2c7998c883b7b0b
Dave Moten <davidmoten@gmail.com>
2014-08-30 06:52:01 +1000

    change selector api to refer to geometry rather than rectangle

--- src/main/java/com/github/davidmoten/rtree/Selector.java
+++ src/main/java/com/github/davidmoten/rtree/Selector.java
@@ -2,15 +2,15 @@ package com.github.davidmoten.rtree;
 
 import java.util.List;
 
-import com.github.davidmoten.rtree.geometry.Rectangle;
+import com.github.davidmoten.rtree.geometry.Geometry;
 
 /**
  * The heuristic used on insert to select which node to add an Entry to.
- *
+ * 
  * @param <T>
  */
 public interface Selector {
 
-    <T> Node<T> select(Rectangle r, List<? extends Node<T>> nodes);
+	<T> Node<T> select(Geometry r, List<? extends Node<T>> nodes);
 
 }
--- src/main/java/com/github/davidmoten/rtree/SelectorMinimalAreaIncrease.java
+++ src/main/java/com/github/davidmoten/rtree/SelectorMinimalAreaIncrease.java
@@ -4,29 +4,32 @@ import static com.google.common.base.Optional.of;
 
 import java.util.List;
 
+import com.github.davidmoten.rtree.geometry.Geometry;
 import com.github.davidmoten.rtree.geometry.Rectangle;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
 public class SelectorMinimalAreaIncrease implements Selector {
 
-    @Override
-    public <T> Node<T> select(Rectangle r, List<? extends Node<T>> nodes) {
-        return findLeastIncreaseInMbrArea(r, nodes);
-    }
+	@Override
+	public <T> Node<T> select(Geometry g, List<? extends Node<T>> nodes) {
+		return findLeastIncreaseInMbrArea(g.mbr(), nodes);
+	}
 
-    private static <T> Node<T> findLeastIncreaseInMbrArea(Rectangle r, List<? extends Node<T>> list) {
-        Preconditions.checkArgument(!list.isEmpty());
-        Optional<Double> minDifference = Optional.absent();
-        Optional<Node<T>> minDiffItem = Optional.absent();
-        for (final Node<T> m : list) {
-            final double diff = m.geometry().mbr().add(r).area() - m.geometry().mbr().area();
-            if (!minDifference.isPresent() || diff < minDifference.get()) {
-                minDifference = of(diff);
-                minDiffItem = of(m);
-            }
-        }
-        return minDiffItem.get();
-    }
+	private static <T> Node<T> findLeastIncreaseInMbrArea(Rectangle r,
+			List<? extends Node<T>> list) {
+		Preconditions.checkArgument(!list.isEmpty());
+		Optional<Double> minDifference = Optional.absent();
+		Optional<Node<T>> minDiffItem = Optional.absent();
+		for (final Node<T> m : list) {
+			final double diff = m.geometry().mbr().add(r).area()
+					- m.geometry().mbr().area();
+			if (!minDifference.isPresent() || diff < minDifference.get()) {
+				minDifference = of(diff);
+				minDiffItem = of(m);
+			}
+		}
+		return minDiffItem.get();
+	}
 
 }
