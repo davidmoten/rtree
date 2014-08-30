fe46149e0dbe9aa1febf0eedbadfa18918cb11b9
Dave Moten <davidmoten@gmail.com>
2014-08-30 12:30:11 +1000

    add point test

---
+++ src/test/java/PointTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Point;
+import com.github.davidmoten.rtree.geometry.Rectangle;
+
+public class PointTest {
+
+	private static final double PRECISION = 0.000001;
+
+	@Test
+	public void testCoordinates() {
+		Point point = Geometries.point(1, 2);
+		assertEquals(1, point.x(), PRECISION);
+		assertEquals(2, point.y(), PRECISION);
+	}
+
+	@Test
+	public void testDistance() {
+		Point p1 = Geometries.point(1, 2);
+		Rectangle r = Geometries.rectangle(4, 6, 4, 6);
+		assertEquals(5, p1.distance(r), PRECISION);
+	}
+}
