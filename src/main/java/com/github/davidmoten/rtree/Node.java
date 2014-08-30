a1509858fc6eaacf517410857ec85e27922705e9
Dave Moten <davidmoten@gmail.com>
2014-08-30 21:48:54 +1000

    add javadoc

--- src/main/java/com/github/davidmoten/rtree/Node.java
+++ src/main/java/com/github/davidmoten/rtree/Node.java
@@ -16,6 +16,15 @@ interface Node<T> extends HasGeometry {
 
     Optional<Node<T>> delete(Entry<T> entry, ImmutableStack<NonLeaf<T>> stack);
 
+    /**
+     * Backpressure method.
+     * 
+     * @param condition
+     * @param subscriber
+     * @param stack
+     * @param request
+     * @return
+     */
     ImmutableStack<NodePosition<T>> search(Func1<? super Geometry, Boolean> condition,
             Subscriber<? super Entry<T>> subscriber, ImmutableStack<NodePosition<T>> stack,
             long request);
