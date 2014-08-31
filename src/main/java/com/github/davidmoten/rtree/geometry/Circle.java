4588e5c72219fd1ac2c86f3e4c17566685bc369f
Dave Moten <davidmoten@gmail.com>
2014-08-31 13:46:36 +1000

    use guava Objects for equals and hashcode

--- src/main/java/com/github/davidmoten/rtree/geometry/Circle.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/Circle.java
@@ -1,70 +1,63 @@
 package com.github.davidmoten.rtree.geometry;
 
+import com.github.davidmoten.util.ObjectsHelper;
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+
 public class Circle implements Geometry {
 
-    private final float x, y, radius;
-    private final Rectangle mbr;
+	private final float x, y, radius;
+	private final Rectangle mbr;
 
-    public Circle(float x, float y, float radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.mbr = Rectangle.create(x - radius, y - radius, x + radius, y + radius);
-    }
+	public Circle(float x, float y, float radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.mbr = Rectangle.create(x - radius, y - radius, x + radius, y
+				+ radius);
+	}
 
-    public static Circle create(double x, double y, double radius) {
-        return new Circle((float) x, (float) y, (float) radius);
-    }
+	public static Circle create(double x, double y, double radius) {
+		return new Circle((float) x, (float) y, (float) radius);
+	}
 
-    public float x() {
-        return x;
-    }
+	public float x() {
+		return x;
+	}
 
-    public float y() {
-        return y;
-    }
+	public float y() {
+		return y;
+	}
 
-    @Override
-    public Rectangle mbr() {
-        return mbr;
-    }
+	@Override
+	public Rectangle mbr() {
+		return mbr;
+	}
 
-    @Override
-    public double distance(Rectangle r) {
-        return Math.max(0, new Point(x, y).distance(r) - radius);
-    }
+	@Override
+	public double distance(Rectangle r) {
+		return Math.max(0, new Point(x, y).distance(r) - radius);
+	}
 
-    @Override
-    public boolean intersects(Rectangle r) {
-        return new Point(x, y).distance(r) <= radius;
-    }
+	@Override
+	public boolean intersects(Rectangle r) {
+		return new Point(x, y).distance(r) <= radius;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Float.floatToIntBits(radius);
-        result = prime * result + Float.floatToIntBits(x);
-        result = prime * result + Float.floatToIntBits(y);
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(x, y, radius);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Circle other = (Circle) obj;
-        if (Float.floatToIntBits(radius) != Float.floatToIntBits(other.radius))
-            return false;
-        if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
-            return false;
-        if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		Optional<Circle> other = ObjectsHelper.asClass(obj, Circle.class);
+		if (other.isPresent()) {
+			return Objects.equal(x, other.get().x)
+					&& Objects.equal(y, other.get().y)
+					&& Objects.equal(radius, other.get().radius);
+		} else
+			return false;
+	}
 
 }
