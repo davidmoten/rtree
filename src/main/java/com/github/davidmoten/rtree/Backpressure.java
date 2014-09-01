6b20505fd11df9cbef0dcf7cdcb2b8461d0555b1
Dave Moten <davidmoten@gmail.com>
2014-09-02 09:15:09 +1000

    simplify backpressure

--- src/main/java/com/github/davidmoten/rtree/Backpressure.java
+++ src/main/java/com/github/davidmoten/rtree/Backpressure.java
@@ -9,47 +9,46 @@ import com.google.common.base.Preconditions;
 
 class Backpressure {
 
-    static <T> ImmutableStack<NodePosition<T>> search(Node<T> node,
-            final Func1<? super Geometry, Boolean> condition,
-            final Subscriber<? super Entry<T>> subscriber, ImmutableStack<NodePosition<T>> stack,
-            long request) {
-        while (true) {
-            Preconditions.checkArgument(!stack.isEmpty());
-            NodePosition<T> np = stack.peek();
-            Preconditions.checkArgument(node == np.node());
-            if (subscriber.isUnsubscribed())
-                return ImmutableStack.empty();
-            else if (request == 0)
-                return stack;
-            else if (np.position() == node.count()) {
-                ImmutableStack<NodePosition<T>> stack2 = stack.pop();
-                if (stack2.isEmpty())
-                    return stack2;
-                else {
-                    NodePosition<T> previous = stack2.peek();
-                    node = previous.node();
-                    stack = stack2.pop().push(previous.nextPosition());
-                }
-            } else if ((node instanceof NonLeaf)) {
-                Node<T> child = ((NonLeaf<T>) node).children().get(np.position());
-                if (condition.call(child.geometry())) {
-                    node = child;
-                    stack = stack.push(new NodePosition<T>(child, 0));
-                } else {
-                    stack = stack.pop().push(np.nextPosition());
-                }
-            } else {
-                Entry<T> entry = ((Leaf<T>) node).entries().get(np.position());
-                final long nextRequest;
-                if (condition.call(entry.geometry())) {
-                    subscriber.onNext(entry);
-                    nextRequest = request - 1;
-                } else
-                    nextRequest = request;
-                stack = stack.pop().push(np.nextPosition());
-                request = nextRequest;
-            }
-        }
-    }
+	static <T> ImmutableStack<NodePosition<T>> search(
+			final Func1<? super Geometry, Boolean> condition,
+			final Subscriber<? super Entry<T>> subscriber,
+			ImmutableStack<NodePosition<T>> stack, long request) {
+		while (true) {
+			Preconditions.checkArgument(!stack.isEmpty());
+			NodePosition<T> np = stack.peek();
+			if (subscriber.isUnsubscribed())
+				return ImmutableStack.empty();
+			else if (request == 0)
+				return stack;
+			else if (np.position() == np.node().count()) {
+				ImmutableStack<NodePosition<T>> stack2 = stack.pop();
+				if (stack2.isEmpty())
+					return stack2;
+				else {
+					NodePosition<T> previous = stack2.peek();
+					stack = stack2.pop().push(previous.nextPosition());
+				}
+			} else if ((np.node() instanceof NonLeaf)) {
+				Node<T> child = ((NonLeaf<T>) np.node()).children().get(
+						np.position());
+				if (condition.call(child.geometry())) {
+					stack = stack.push(new NodePosition<T>(child, 0));
+				} else {
+					stack = stack.pop().push(np.nextPosition());
+				}
+			} else {
+				Entry<T> entry = ((Leaf<T>) np.node()).entries().get(
+						np.position());
+				final long nextRequest;
+				if (condition.call(entry.geometry())) {
+					subscriber.onNext(entry);
+					nextRequest = request - 1;
+				} else
+					nextRequest = request;
+				stack = stack.pop().push(np.nextPosition());
+				request = nextRequest;
+			}
+		}
+	}
 
 }
--- src/main/java/com/github/davidmoten/rtree/OnSubscribeSearch.java
+++ src/main/java/com/github/davidmoten/rtree/OnSubscribeSearch.java
@@ -12,74 +12,76 @@ import com.github.davidmoten.util.ImmutableStack;
 
 final class OnSubscribeSearch<T> implements OnSubscribe<Entry<T>> {
 
-    private final Node<T> node;
-    private final Func1<? super Geometry, Boolean> condition;
+	private final Node<T> node;
+	private final Func1<? super Geometry, Boolean> condition;
 
-    OnSubscribeSearch(Node<T> node, Func1<? super Geometry, Boolean> condition) {
-        this.node = node;
-        this.condition = condition;
-    }
+	OnSubscribeSearch(Node<T> node, Func1<? super Geometry, Boolean> condition) {
+		this.node = node;
+		this.condition = condition;
+	}
 
-    @Override
-    public void call(Subscriber<? super Entry<T>> subscriber) {
-        subscriber.setProducer(new SearchProducer<T>(node, condition, subscriber));
-    }
+	@Override
+	public void call(Subscriber<? super Entry<T>> subscriber) {
+		subscriber.setProducer(new SearchProducer<T>(node, condition,
+				subscriber));
+	}
 
-    private static class SearchProducer<T> implements Producer {
+	private static class SearchProducer<T> implements Producer {
 
-        private final Subscriber<? super Entry<T>> subscriber;
-        private final Node<T> node;
-        private final Func1<? super Geometry, Boolean> condition;
-        private volatile ImmutableStack<NodePosition<T>> stack;
-        private final AtomicLong requested = new AtomicLong(0);
+		private final Subscriber<? super Entry<T>> subscriber;
+		private final Node<T> node;
+		private final Func1<? super Geometry, Boolean> condition;
+		private volatile ImmutableStack<NodePosition<T>> stack;
+		private final AtomicLong requested = new AtomicLong(0);
 
-        public SearchProducer(Node<T> node, Func1<? super Geometry, Boolean> condition,
-                Subscriber<? super Entry<T>> subscriber) {
-            this.node = node;
-            this.condition = condition;
-            this.subscriber = subscriber;
-            stack = ImmutableStack.create(new NodePosition<T>(node, 0));
-        }
+		public SearchProducer(Node<T> node,
+				Func1<? super Geometry, Boolean> condition,
+				Subscriber<? super Entry<T>> subscriber) {
+			this.node = node;
+			this.condition = condition;
+			this.subscriber = subscriber;
+			stack = ImmutableStack.create(new NodePosition<T>(node, 0));
+		}
 
-        @Override
-        public void request(long n) {
-            if (requested.get() == Long.MAX_VALUE)
-                // already started with fast path
-                return;
-            else if (n == Long.MAX_VALUE) {
-                // fast path
-                requestAll();
-            } else {
-                requestSome(n);
-            }
-        }
+		@Override
+		public void request(long n) {
+			if (requested.get() == Long.MAX_VALUE)
+				// already started with fast path
+				return;
+			else if (n == Long.MAX_VALUE) {
+				// fast path
+				requestAll();
+			} else {
+				requestSome(n);
+			}
+		}
 
-        private void requestAll() {
-            node.search(condition, subscriber);
-            if (!subscriber.isUnsubscribed())
-                subscriber.onCompleted();
-        }
+		private void requestAll() {
+			node.search(condition, subscriber);
+			if (!subscriber.isUnsubscribed())
+				subscriber.onCompleted();
+		}
 
-        public void requestSome(long n) {
-            // back pressure path
-            // this algorithm copied roughly from
-            // rxjava-core/OnSubscribeFromIterable.java
-            long previousCount = requested.getAndAdd(n);
-            if (previousCount == 0) {
-                while (true) {
-                    long r = requested.get();
-                    long numToEmit = r;
+		public void requestSome(long n) {
+			// back pressure path
+			// this algorithm copied roughly from
+			// rxjava-core/OnSubscribeFromIterable.java
+			long previousCount = requested.getAndAdd(n);
+			if (previousCount == 0) {
+				while (true) {
+					long r = requested.get();
+					long numToEmit = r;
 
-                    stack = Backpressure.search(stack.peek().node(), condition, subscriber, stack,
-                            numToEmit);
-                    if (stack.isEmpty()) {
-                        if (!subscriber.isUnsubscribed())
-                            subscriber.onCompleted();
-                    } else if (requested.addAndGet(-r) == 0)
-                        return;
-                }
-            }
-        }
-    }
+					stack = Backpressure.search(condition, subscriber, stack,
+							numToEmit);
+					if (stack.isEmpty()) {
+						if (!subscriber.isUnsubscribed())
+							subscriber.onCompleted();
+					} else if (requested.addAndGet(-r) == 0)
+						return;
+				}
+			}
+		}
+	}
 
 }
