e7e50048c8892e4cf49cb54b1ddc7ea02a013708
Dave Moten <davidmoten@gmail.com>
2014-09-03 21:39:21 +1000

    fix the search benchmark

--- src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
+++ src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
@@ -4,10 +4,15 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Point;
+
 @State(Scope.Benchmark)
 public class BenchmarksRTree {
 
 	private final RTree<Object> tree = RTreeTest.createRandomRTree(10000);
+
+	private final RTree<Object> tree2 = createTree();
 
 	@Benchmark
 	public void createRTreeAndInsertOneEntryInto10000Entries() {
@@ -15,8 +20,23 @@ public class BenchmarksRTree {
 	}
 
 	@Benchmark
-	public void searchRTreeOf10000EntriesForOneRandomEntry() {
-		tree.search(RTreeTest.random()).count().toBlocking().single();
+	public void searchRTreeOf10000PointsUsingSmallishRectangle() {
+		tree2.search(Geometries.rectangle(500, 500, 510, 510)).count()
+				.toBlocking().single();
+	}
+
+	private static final RTree<Object> createTree() {
+		RTree<Object> tree = RTree.maxChildren(10).create();
+		for (int i = 0; i < 10000; i++) {
+			tree = tree.add(new Object(), createPoint());
+		}
+		return tree;
+	}
+
+	private static final Point createPoint() {
+		double x = Math.random() * 1000;
+		double y = Math.random() * 1000;
+		return Geometries.point(x, y);
 	}
 
 }
