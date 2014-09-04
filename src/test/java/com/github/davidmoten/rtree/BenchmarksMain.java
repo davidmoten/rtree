924ba48f62df545db8281ab35e719ea01a137bdc
Dave Moten <davidmoten@gmail.com>
2014-09-04 18:07:17 +1000

    shorten benchmark

--- src/test/java/com/github/davidmoten/rtree/BenchmarksMain.java
+++ src/test/java/com/github/davidmoten/rtree/BenchmarksMain.java
@@ -2,27 +2,26 @@ package com.github.davidmoten.rtree;
 
 import java.io.IOException;
 
-import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.Main;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchmarksMain {
 
 	public static void main(String[] args) throws RunnerException, IOException {
-		Options opt = new OptionsBuilder()
-		//
-				.include(BenchmarksRTree.class.getSimpleName())
-				//
-				.forks(1)
-				//
-				.warmupIterations(10)
-				//
-				.measurementIterations(10)
-				//
-				.jvmArgs("-Xmx512m")
-				//
-				.build();
-		new Runner(opt).run();
+		Main.main(args);
+		// Options opt = new OptionsBuilder()
+		// //
+		// .include(BenchmarksRTree.class.getSimpleName())
+		// //
+		// .forks(1)
+		// //
+		// .warmupIterations(10)
+		// //
+		// .measurementIterations(10)
+		// //
+		// .jvmArgs("-Xmx512m")
+		// //
+		// .build();
+		// new Runner(opt).run();
 	}
 }
