ce4074889b29dafd2bd029cfa01c520da6663a68
Dave Moten <davidmoten@gmail.com>
2014-09-05 16:49:47 +1000

    add R*-tree benchmarks

--- src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
+++ src/test/java/com/github/davidmoten/rtree/BenchmarksRTree.java
@@ -10,35 +10,35 @@ import com.github.davidmoten.rtree.geometry.Point;
 @State(Scope.Benchmark)
 public class BenchmarksRTree {
 
-	private final RTree<Object> tree = createTree();
+	private final RTree<Object> tree = createTree(100000);
 
 	private final RTree<Object> starTree = RTree.maxChildren(4).star().create();
 
 	@Benchmark
-	public void defaultRTreeInsertOneEntryInto10000Entries() {
+	public void defaultRTreeInsertOneEntryInto100KEntries() {
 		tree.add(new Object(), RTreeTest.random());
 	}
 
 	@Benchmark
-	public void defaultRTreeSearchOf10000PointsUsingSmallishRectangle() {
+	public void defaultRTreeSearchOf100KPointsUsingSmallishRectangle() {
 		tree.search(Geometries.rectangle(500, 500, 510, 510)).count()
 				.toBlocking().single();
 	}
 
 	@Benchmark
-	public void rStarTreeInsertOneEntryInto10000Entries() {
+	public void rStarTreeInsertOneEntryInto100KEntries() {
 		starTree.add(new Object(), RTreeTest.random());
 	}
 
 	@Benchmark
-	public void rStarTreeSearchOf10000PointsUsingSmallishRectangle() {
+	public void rStarTreeSearchOf100KPointsUsingSmallishRectangle() {
 		starTree.search(Geometries.rectangle(500, 500, 510, 510)).count()
 				.toBlocking().single();
 	}
 
-	private static final RTree<Object> createTree() {
+	private static final RTree<Object> createTree(long n) {
 		RTree<Object> tree = RTree.maxChildren(10).create();
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 0; i < n; i++) {
 			tree = tree.add(new Object(), createPoint());
 		}
 		return tree;
