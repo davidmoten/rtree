7beb36aa6bd47c6b33734df6b0231f6163533fd0
Dave Moten <davidmoten@gmail.com>
2014-09-07 22:22:23 +1000

    update README, change modifiers

--- src/main/java/com/github/davidmoten/rtree/Comparators.java
+++ src/main/java/com/github/davidmoten/rtree/Comparators.java
@@ -11,60 +11,59 @@ import com.github.davidmoten.rtree.geometry.Rectangle;
 
 public final class Comparators {
 
-	public static final Comparator<ListPair<?>> overlapListPairComparator = toComparator(Functions.overlapListPair);
+    public static final Comparator<ListPair<?>> overlapListPairComparator = toComparator(Functions.overlapListPair);
 
-	public static Comparator<ListPair<?>> areaPairComparator = new Comparator<ListPair<?>>() {
+    public static Comparator<ListPair<?>> areaPairComparator = new Comparator<ListPair<?>>() {
 
-		@Override
-		public int compare(ListPair<?> p1, ListPair<?> p2) {
-			return ((Float) p1.areaSum()).compareTo(p2.areaSum());
-		}
-	};
+        @Override
+        public int compare(ListPair<?> p1, ListPair<?> p2) {
+            return ((Float) p1.areaSum()).compareTo(p2.areaSum());
+        }
+    };
 
-	public static <T extends HasGeometry> Comparator<HasGeometry> overlapComparator(
-			final Rectangle r, final List<T> list) {
-		return toComparator(Functions.overlap(r, list));
-	}
+    public static <T extends HasGeometry> Comparator<HasGeometry> overlapComparator(
+            final Rectangle r, final List<T> list) {
+        return toComparator(Functions.overlap(r, list));
+    }
 
-	public static <T extends HasGeometry> Comparator<HasGeometry> areaIncreaseComparator(
-			final Rectangle r) {
-		return toComparator(Functions.areaIncrease(r));
-	}
+    public static <T extends HasGeometry> Comparator<HasGeometry> areaIncreaseComparator(
+            final Rectangle r) {
+        return toComparator(Functions.areaIncrease(r));
+    }
 
-	public static Comparator<HasGeometry> areaComparator(final Rectangle r) {
-		return new Comparator<HasGeometry>() {
+    public static Comparator<HasGeometry> areaComparator(final Rectangle r) {
+        return new Comparator<HasGeometry>() {
 
-			@Override
-			public int compare(HasGeometry g1, HasGeometry g2) {
-				return ((Float) g1.geometry().mbr().add(r).area()).compareTo(g2
-						.geometry().mbr().add(r).area());
-			}
-		};
-	}
+            @Override
+            public int compare(HasGeometry g1, HasGeometry g2) {
+                return ((Float) g1.geometry().mbr().add(r).area()).compareTo(g2.geometry().mbr()
+                        .add(r).area());
+            }
+        };
+    }
 
-	public static <R, T extends Comparable<T>> Comparator<R> toComparator(
-			final Func1<R, T> function) {
-		return new Comparator<R>() {
+    public static <R, T extends Comparable<T>> Comparator<R> toComparator(final Func1<R, T> function) {
+        return new Comparator<R>() {
 
-			@Override
-			public int compare(R g1, R g2) {
-				return function.call(g1).compareTo(function.call(g2));
-			}
-		};
-	}
+            @Override
+            public int compare(R g1, R g2) {
+                return function.call(g1).compareTo(function.call(g2));
+            }
+        };
+    }
 
-	public static <T> Comparator<T> compose(final Comparator<T>... comparators) {
-		return new Comparator<T>() {
-			@Override
-			public int compare(T t1, T t2) {
-				for (Comparator<T> comparator : comparators) {
-					int value = comparator.compare(t1, t2);
-					if (value != 0)
-						return value;
-				}
-				return 0;
-			}
-		};
-	}
+    public static <T> Comparator<T> compose(final Comparator<T>... comparators) {
+        return new Comparator<T>() {
+            @Override
+            public int compare(T t1, T t2) {
+                for (Comparator<T> comparator : comparators) {
+                    int value = comparator.compare(t1, t2);
+                    if (value != 0)
+                        return value;
+                }
+                return 0;
+            }
+        };
+    }
 
 }
--- src/main/java/com/github/davidmoten/rtree/Functions.java
+++ src/main/java/com/github/davidmoten/rtree/Functions.java
@@ -10,41 +10,41 @@ import com.github.davidmoten.rtree.geometry.Rectangle;
 
 public class Functions {
 
-	public static Func1<ListPair<? extends HasGeometry>, Double> overlapListPair = new Func1<ListPair<? extends HasGeometry>, Double>() {
+    static Func1<ListPair<? extends HasGeometry>, Double> overlapListPair = new Func1<ListPair<? extends HasGeometry>, Double>() {
 
-		@Override
-		public Double call(ListPair<? extends HasGeometry> pair) {
-			return (double) pair.group1().geometry().mbr()
-					.intersectionArea(pair.group2().geometry().mbr());
-		}
-	};
+        @Override
+        public Double call(ListPair<? extends HasGeometry> pair) {
+            return (double) pair.group1().geometry().mbr()
+                    .intersectionArea(pair.group2().geometry().mbr());
+        }
+    };
 
-	public static Func1<HasGeometry, Double> overlap(final Rectangle r,
-			final List<? extends HasGeometry> list) {
-		return new Func1<HasGeometry, Double>() {
+    public static Func1<HasGeometry, Double> overlap(final Rectangle r,
+            final List<? extends HasGeometry> list) {
+        return new Func1<HasGeometry, Double>() {
 
-			@Override
-			public Double call(HasGeometry g) {
-				Rectangle gPlusR = g.geometry().mbr().add(r);
-				double m = 0;
-				for (HasGeometry other : list) {
-					if (other != g) {
-						m += gPlusR.intersectionArea(other.geometry().mbr());
-					}
-				}
-				return m;
-			}
-		};
-	}
+            @Override
+            public Double call(HasGeometry g) {
+                Rectangle gPlusR = g.geometry().mbr().add(r);
+                double m = 0;
+                for (HasGeometry other : list) {
+                    if (other != g) {
+                        m += gPlusR.intersectionArea(other.geometry().mbr());
+                    }
+                }
+                return m;
+            }
+        };
+    }
 
-	public static Func1<HasGeometry, Double> areaIncrease(final Rectangle r) {
-		return new Func1<HasGeometry, Double>() {
-			@Override
-			public Double call(HasGeometry g) {
-				Rectangle gPlusR = g.geometry().mbr().add(r);
-				return (double) (gPlusR.area() - g.geometry().mbr().area());
-			}
-		};
-	}
+    public static Func1<HasGeometry, Double> areaIncrease(final Rectangle r) {
+        return new Func1<HasGeometry, Double>() {
+            @Override
+            public Double call(HasGeometry g) {
+                Rectangle gPlusR = g.geometry().mbr().add(r);
+                return (double) (gPlusR.area() - g.geometry().mbr().area());
+            }
+        };
+    }
 
 }
