694c0edbe22970fd1ff18f7546fc88720198db74
Dave Moten <davidmoten@gmail.com>
2014-09-15 17:34:03 +1000

    add javadoc

--- src/main/java/com/github/davidmoten/rtree/NodeAndEntries.java
+++ src/main/java/com/github/davidmoten/rtree/NodeAndEntries.java
@@ -4,12 +4,31 @@ import java.util.List;
 
 import com.google.common.base.Optional;
 
+/**
+ * Used for tracking deletions through recursive calls.
+ * 
+ * @param <T>
+ *            entry type
+ */
 public class NodeAndEntries<T> {
 
     private final Optional<? extends Node<T>> node;
     private final List<Entry<T>> entries;
     private final int count;
 
+    /**
+     * Constructor.
+     * 
+     * @param node
+     *            absent = whole node was deleted present = either an unchanged
+     *            node because of no removal or the newly created node without
+     *            the deleted entry
+     * @param entries
+     *            from nodes that dropped below minChildren in size and thus
+     *            their entries are to be redistributed (readded to the tree)
+     * @param countDeleted
+     *            count of the number of entries removed
+     */
     public NodeAndEntries(Optional<? extends Node<T>> node, List<Entry<T>> entries, int countDeleted) {
         this.node = node;
         this.entries = entries;
