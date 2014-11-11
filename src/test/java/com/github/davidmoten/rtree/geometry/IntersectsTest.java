a96a9adcb34729fffa39a298c92cf236f71b31e9
Dave Moten <davidmoten@gmail.com>
2014-11-11 15:46:04 +1100

    add tests for Intersects

--- src/test/java/com/github/davidmoten/rtree/geometry/IntersectsTest.java
+++ src/test/java/com/github/davidmoten/rtree/geometry/IntersectsTest.java
@@ -1,5 +1,10 @@
 package com.github.davidmoten.rtree.geometry;
 
+import static com.github.davidmoten.rtree.geometry.Geometries.circle;
+import static com.github.davidmoten.rtree.geometry.Geometries.rectangle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.github.davidmoten.util.TestingUtil;
@@ -9,6 +14,16 @@ public class IntersectsTest {
     @Test
     public void testConstructorIsPrivate() {
         TestingUtil.callConstructorAndCheckIsPrivate(Intersects.class);
+    }
+    
+    @Test
+    public void testRectangleIntersectsCircle() {
+        assertTrue(Intersects.rectangleIntersectsCircle.call(rectangle(0, 0, 0, 0), circle(0, 0, 1)));
+    }
+    
+    @Test
+    public void testRectangleDoesNotIntersectCircle() {
+        assertFalse(Intersects.rectangleIntersectsCircle.call(rectangle(0, 0, 0, 0), circle(100, 100, 1)));
     }
     
 }
