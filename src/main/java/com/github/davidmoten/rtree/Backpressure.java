4e9c9a3d249b85ae9a741fa9dd9b17b114cb7bf1
Dave Moten <davidmoten@gmail.com>
2014-11-13 08:54:54 +1100

    refactor Backpressure to reduce ncss in method

--- src/main/java/com/github/davidmoten/rtree/Backpressure.java
+++ src/main/java/com/github/davidmoten/rtree/Backpressure.java
@@ -19,44 +19,63 @@ final class Backpressure {
             final Func1<? super Geometry, Boolean> condition,
             final Subscriber<? super Entry<T, S>> subscriber,
             ImmutableStack<NodePosition<T, S>> stack, long request) {
-        if (stack.isEmpty())
-            return stack;
-        while (true) {
+        while (!stack.isEmpty()) {
             NodePosition<T, S> np = stack.peek();
             if (subscriber.isUnsubscribed())
                 return ImmutableStack.empty();
             else if (request == 0)
                 return stack;
             else if (np.position() == np.node().count()) {
-                // handle after last position in node
-                ImmutableStack<NodePosition<T, S>> stack2 = stack.pop();
-                if (stack2.isEmpty())
-                    return stack2;
-                else {
-                    NodePosition<T, S> previous = stack2.peek();
-                    stack = stack2.pop().push(previous.nextPosition());
-                }
+                // handle after last in node
+                stack = searchAfterLastInNode(stack);
             } else if (np.node() instanceof NonLeaf) {
                 // handle non-leaf
-                Node<T, S> child = ((NonLeaf<T, S>) np.node()).children().get(np.position());
-                if (condition.call(child.geometry())) {
-                    stack = stack.push(new NodePosition<T, S>(child, 0));
-                } else {
-                    stack = stack.pop().push(np.nextPosition());
-                }
+                stack = searchNonLeaf(condition, stack, np);
             } else {
                 // handle leaf
-                Entry<T, S> entry = ((Leaf<T, S>) np.node()).entries().get(np.position());
-                final long nextRequest;
-                if (condition.call(entry.geometry())) {
-                    subscriber.onNext(entry);
-                    nextRequest = request - 1;
-                } else
-                    nextRequest = request;
+                long nextRequest = searchLeaf(condition, subscriber, request, np);
                 stack = stack.pop().push(np.nextPosition());
                 request = nextRequest;
             }
         }
+        return stack;
+    }
+
+    private static <T, S extends Geometry> long searchLeaf(
+            final Func1<? super Geometry, Boolean> condition,
+            final Subscriber<? super Entry<T, S>> subscriber, long request, NodePosition<T, S> np) {
+        final long nextRequest;
+        Entry<T, S> entry = ((Leaf<T, S>) np.node()).entries().get(np.position());
+        if (condition.call(entry.geometry())) {
+            subscriber.onNext(entry);
+            nextRequest = request - 1;
+        } else
+            nextRequest = request;
+        return nextRequest;
+    }
+
+    private static <S extends Geometry, T> ImmutableStack<NodePosition<T, S>> searchAfterLastInNode(
+            ImmutableStack<NodePosition<T, S>> stack) {
+        ImmutableStack<NodePosition<T, S>> stack2 = stack.pop();
+        if (stack2.isEmpty())
+            stack = stack2;
+        else {
+            NodePosition<T, S> previous = stack2.peek();
+            stack = stack2.pop().push(previous.nextPosition());
+        }
+        return stack;
+    }
+
+    private static <S extends Geometry, T> ImmutableStack<NodePosition<T, S>> searchNonLeaf(
+            final Func1<? super Geometry, Boolean> condition,
+            ImmutableStack<NodePosition<T, S>> stack, NodePosition<T, S> np) {
+        Node<T, S> child = ((NonLeaf<T, S>) np.node()).children().get(np.position());
+        if (condition.call(child.geometry())) {
+            stack = stack.push(new NodePosition<T, S>(child, 0));
+        } else {
+            stack = stack.pop().push(np.nextPosition());
+        }
+        return stack;
     }
 
 }
