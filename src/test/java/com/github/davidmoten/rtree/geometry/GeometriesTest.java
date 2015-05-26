4dca0bcc8612427ff4bf9ba4c3350a3c1ad12318
Dave Moten <davidmoten@gmail.com>
2015-05-26 19:47:59 +1000

    add unit tests for factory methods

--- src/test/java/com/github/davidmoten/rtree/geometry/GeometriesTest.java
+++ src/test/java/com/github/davidmoten/rtree/geometry/GeometriesTest.java
@@ -1,14 +1,76 @@
 package com.github.davidmoten.rtree.geometry;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.github.davidmoten.util.TestingUtil;
 
 public class GeometriesTest {
 
+    private static final double PRECISION = 0.000001;
+
     @Test
     public void testPrivateConstructorForCoverageOnly() {
         TestingUtil.callConstructorAndCheckIsPrivate(Geometries.class);
     }
 
+    @Test
+    public void testNormalizeLongitude() {
+        assertEquals(0, Geometries.normalizeLongitude(0), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude2() {
+        assertEquals(89, Geometries.normalizeLongitude(89), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude3() {
+        assertEquals(179, Geometries.normalizeLongitude(179), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude4() {
+        assertEquals(-179, Geometries.normalizeLongitude(181), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude5() {
+        assertEquals(-179, Geometries.normalizeLongitude(541), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude2Neg() {
+        assertEquals(-89, Geometries.normalizeLongitude(-89), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude3Neg() {
+        assertEquals(-179, Geometries.normalizeLongitude(-179), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude4Neg() {
+        assertEquals(179, Geometries.normalizeLongitude(-181), PRECISION);
+    }
+
+    @Test
+    public void testNormalizeLongitude5Neg() {
+        assertEquals(179, Geometries.normalizeLongitude(-541), PRECISION);
+    }
+
+    @Test
+    public void testRectangleLatLong() {
+        Rectangle r = Geometries.rectangleLatLong(10, -10, 5, 10);
+        assertEquals(10, r.x1(), PRECISION);
+        assertEquals(365, r.x2(), PRECISION);
+    }
+
+    @Test
+    public void testRectangleLatLong2() {
+        Rectangle r = Geometries.rectangleLatLong(5, -10, 10, 10);
+        assertEquals(5, r.x1(), PRECISION);
+        assertEquals(10, r.x2(), PRECISION);
+    }
 }
