dcd64345dde5bd06567dacc0204092e10563fc6d
Dave Moten <davidmoten@gmail.com>
2015-05-26 19:00:34 +1000

    add factory methods for lat lon rectangles and points

--- src/main/java/com/github/davidmoten/rtree/geometry/Geometries.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/Geometries.java
@@ -1,5 +1,7 @@
 package com.github.davidmoten.rtree.geometry;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public final class Geometries {
 
     private Geometries() {
@@ -17,5 +19,27 @@ public final class Geometries {
     public static Circle circle(double x, double y, double radius) {
         return Circle.create(x, y, radius);
     }
-    
+
+    public static Rectangle rectangleLatLong(double lon1, double lat1, double lon2, double lat2) {
+        double x1 = normalizeLongitude(lon1);
+        double x2 = normalizeLongitude(lon2);
+        if (x2 < x1) {
+            x2 += 360;
+        }
+        return rectangle(x1, lat1, x2, lon2);
+    }
+
+    public static Point pointLatLong(double lon, double lat) {
+        return point(normalizeLongitude(lon), lat);
+    }
+
+    @VisibleForTesting
+    static double normalizeLongitude(double d) {
+        double sign = Math.signum(d);
+        double x = Math.abs(d) / 360;
+        double x2 = (x - Math.floor(x)) * 360;
+        if (x2 >= 180)
+            x2 -= 360;
+        return x2 * sign;
+    }
 }
