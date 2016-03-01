4c8a7313ca263efb2d76c7121d579c0a0f235ab7
Dave Moten <davidmoten@gmail.com>
2016-03-01 16:34:27 +1100

    add generated classes to src/main/java

---
+++ src/main/java/com/github/davidmoten/rtree/fbs/generated/Point_.java
@@ -0,0 +1,24 @@
+// automatically generated, do not modify
+
+package com.github.davidmoten.rtree.fbs.generated;
+
+import java.nio.*;
+import java.lang.*;
+import java.util.*;
+import com.google.flatbuffers.*;
+
+@SuppressWarnings("unused")
+public final class Point_ extends Struct {
+  public Point_ __init(int _i, ByteBuffer _bb) { bb_pos = _i; bb = _bb; return this; }
+
+  public float x() { return bb.getFloat(bb_pos + 0); }
+  public float y() { return bb.getFloat(bb_pos + 4); }
+
+  public static int createPoint_(FlatBufferBuilder builder, float x, float y) {
+    builder.prep(4, 8);
+    builder.putFloat(y);
+    builder.putFloat(x);
+    return builder.offset();
+  }
+};
+
