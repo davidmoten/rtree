7d11c074d205968e1b1332850dcd73e1af912ad1
Dave Moten <davidmoten@gmail.com>
2016-03-05 14:28:40 +1100

    fix warning

--- src/test/java/com/github/davidmoten/rtree/NodePositionTest.java
+++ src/test/java/com/github/davidmoten/rtree/NodePositionTest.java
@@ -5,12 +5,15 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import com.github.davidmoten.rtree.geometry.Rectangle;
+
 public class NodePositionTest {
 
     @Test
-    public void test() {
-        Node node = Mockito.mock(Node.class);
-        assertTrue(new NodePosition(node, 1).toString().startsWith("NodePosition ["));
+    public void testToString() {
+        @SuppressWarnings("unchecked")
+        Node<Object,Rectangle> node = Mockito.mock(Node.class);
+        assertTrue(new NodePosition<Object,Rectangle>(node, 1).toString().startsWith("NodePosition ["));
     }
     
 }
