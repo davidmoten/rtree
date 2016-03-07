7d18f45e946a1bb869abc00cdc442487258f5a54
Dave Moten <davidmoten@gmail.com>
2016-03-08 09:17:48 +1100

    add javadoc

--- src/main/java/com/github/davidmoten/rtree/Leaf.java
+++ src/main/java/com/github/davidmoten/rtree/Leaf.java
@@ -7,7 +7,16 @@ import com.github.davidmoten.rtree.geometry.Geometry;
 public interface Leaf<T, S extends Geometry> extends Node<T, S> {
 
     List<Entry<T, S>> entries();
-    
+
+    /**
+     * Returns the ith entry (0-based). This method should be preferred for
+     * performance reasons when only one entry is required (in comparison to
+     * {@code entries().get(i)}).
+     * 
+     * @param i
+     *            0-based index
+     * @return ith entry
+     */
     Entry<T, S> entry(int i);
 
 }
