fac2ed6ab2a71b50ac924bc4e23a59b032cd7966
Dave Moten <davidmoten@gmail.com>
2016-05-05 18:31:13 +1000

    add kryo serialization test

--- src/test/java/com/github/davidmoten/rtree/KryoSerializationTest.java
+++ src/test/java/com/github/davidmoten/rtree/KryoSerializationTest.java
@@ -1,5 +1,7 @@
 package com.github.davidmoten.rtree;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
@@ -25,6 +27,33 @@ public class KryoSerializationTest {
         output.close();
         Input input = new Input(new ByteArrayInputStream(bytes.toByteArray()));
         RTree<String, Point> tree2 = kryo.readObject(input, RTree.class);
+        assertEquals(2, (int) tree2.entries().count().toBlocking().single());
+    }
+
+    @Test
+    public void testKryo() {
+        Kryo kryo = new Kryo();
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        Output output = new Output(bytes);
+        Boo b = new Boo("hello");
+        kryo.writeObject(output, b);
+        output.close();
+        Input input = new Input(new ByteArrayInputStream(bytes.toByteArray()));
+        Boo b2 = kryo.readObject(input, Boo.class);
+        assertEquals("hello", b2.name);
+    }
+
+    public static class Boo {
+
+        public final String name;
+
+        private Boo() {
+            this("boo");
+        }
+
+        public Boo(String name) {
+            this.name = name;
+        }
     }
 
 }
