db3ca7d58c948073025d4a6485af6dd7c07f20e2
Dave Moten <davidmoten@gmail.com>
2016-06-28 18:47:50 +1000

    dev add kryo

--- src/main/java/com/github/davidmoten/rtree/kryo/SerializerKryo.java
+++ src/main/java/com/github/davidmoten/rtree/kryo/SerializerKryo.java
@@ -5,19 +5,21 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.github.davidmoten.rtree.Context;
 import com.github.davidmoten.rtree.InternalStructure;
+import com.github.davidmoten.rtree.Leaf;
 import com.github.davidmoten.rtree.Node;
 import com.github.davidmoten.rtree.RTree;
-import com.github.davidmoten.rtree.Serializer;
 import com.github.davidmoten.rtree.geometry.Geometry;
+import com.github.davidmoten.rtree.geometry.Rectangle;
 
 import rx.functions.Func0;
 import rx.functions.Func1;
 
-public class SerializerKryo<T, S extends Geometry> implements Serializer<T, S> {
+public class SerializerKryo<T, S extends Geometry> extends Serializer<RTree<T,S>> {
 
     private final Func1<? super T, byte[]> serializer;
     private final Func1<byte[], ? extends T> deserializer;
@@ -31,17 +33,17 @@ public class SerializerKryo<T, S extends Geometry> implements Serializer<T, S> {
     }
 
     @Override
-    public void write(RTree<T, S> tree, OutputStream os) throws IOException {
-        Kryo k = kryoFactory.call();
+    public RTree<T, S> read(Kryo arg0, Input arg1, Class<RTree<T, S>> arg2) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
-    @Override
-    public RTree<T, S> read(InputStream is, long sizeBytes, InternalStructure structure)
-            throws IOException {
-        Kryo k = kryoFactory.call();
-        // k.register(RTree.class, RTreeSerializer.class);
 
-        return null;
+
+    @Override
+    public void write(Kryo arg0, Output arg1, RTree<T, S> arg2) {
+        // TODO Auto-generated method stub
+        
     }
 
     public static <T, S extends Geometry> Serializer<T, S> create(
@@ -63,8 +65,20 @@ public class SerializerKryo<T, S extends Geometry> implements Serializer<T, S> {
         }
 
         private void writeNode(Node<T, S> node, Output output) {
-            // TODO Auto-generated method stub
+            boolean isLeaf = node instanceof Leaf;
+            output.writeBoolean(isLeaf);
+            if (isLeaf) {
+                Leaf<T, S> leaf = (Leaf<T, S>) node;
+                output.writeInt(leaf.count());
+                writeBounds(leaf.geometry().mbr(), output);
+            }
+        }
 
+        private void writeBounds(Rectangle mbr, Output output) {
+            output.writeFloat(mbr.x1());
+            output.writeFloat(mbr.y1());
+            output.writeFloat(mbr.y1());
+            output.writeFloat(mbr.y2());
         }
 
         private void writeContext(Context<T, S> context, Output output) {
@@ -80,4 +94,5 @@ public class SerializerKryo<T, S extends Geometry> implements Serializer<T, S> {
         }
 
     }
+    
 }
