15d37ce85605c2e1fa4777f4b88ffed947922312
Dave Moten <davidmoten@gmail.com>
2017-12-16 07:35:59 +1100

    add high precision test

---
+++ src/test/java/com/github/davidmoten/rtree/HighPrecisionTest.java
@@ -0,0 +1,34 @@
+package com.github.davidmoten.rtree;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import org.junit.Test;
+
+public class HighPrecisionTest {
+
+    @Test
+    public void testForIssue72() {
+        long x = 123456789L;
+        System.out.println(new BigDecimal(x).floatValue());
+        BigDecimal b = new BigDecimal(x);
+        System.out.println(b.round(FLOOR).floatValue());
+        System.out.println(b.round(CEILING).floatValue());
+    }
+
+    private static final MathContext FLOOR = new MathContext(7, RoundingMode.FLOOR);
+    private static final MathContext CEILING = new MathContext(7, RoundingMode.CEILING);
+
+    private static float floor(long x) {
+        return new BigDecimal(x).round(FLOOR).floatValue();
+    }
+
+    private static float ceil(long x) {
+        return new BigDecimal(x).round(CEILING).floatValue();
+    }
+
+    private static boolean gte(float a, long b) {
+        return new BigDecimal(a).compareTo(new BigDecimal(b)) >=0;
+    }
+}
