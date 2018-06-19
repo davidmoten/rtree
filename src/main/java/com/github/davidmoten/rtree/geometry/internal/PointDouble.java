e71308f0598ae973ddf6a7bb8ec6a0fb3d9b986e
Dave Moten <davidmoten@gmail.com>
2018-06-19 17:20:38 +1000

    fix deletion of PointDouble by implementing hashCode and equals #81

--- src/main/java/com/github/davidmoten/rtree/geometry/internal/PointDouble.java
+++ src/main/java/com/github/davidmoten/rtree/geometry/internal/PointDouble.java
@@ -105,4 +105,32 @@ public final class PointDouble implements Point {
         return true;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PointDouble other = (PointDouble) obj;
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+            return false;
+        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+            return false;
+        return true;
+    }
+
 }
